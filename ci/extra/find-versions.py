f2769ec87438231a79f03404eea89e782548d88b
Francesco Romani <fromani@redhat.com>
2019-10-01 10:50:31 +0200

    versions: add helpers to get kubevirt versions
    Add helper to get, given a kubevirt version (either
    explicitely or from go.mod):
    1. the previous
    2. the following
    3. the secondlast (aka penultimate)
    4. the last
    
    Will be useful for CI to test upgrades
    
    Signed-off-by: Francesco Romani <fromani@redhat.com>

---
+++ ci/extra/find-versions.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import collections
+import sys
+import json
+
+
+_version = collections.namedtuple('_version', ['major', 'minor', 'micro'])
+
+
+class Version(_version):
+
+    @classmethod
+    def from_string(cls, text):
+        text = text.lstrip("v")
+        text = text.split("-")[0]  # TODO: handle '-extra'?
+        major, minor, micro = text.split('.')
+        return cls(int(major), int(minor), int(micro))
+
+    def __str__(self):
+        return "v%d.%d.%d" % (self.major, self.minor, self.micro)
+
+
+def versions(data):
+    tags = [ item['tag_name'] for item in data ]
+    versions = [ Version.from_string(tag) for tag in tags]
+    buckets = {}
+    for ver in versions:
+        key = (ver.major, ver.minor)
+        if key not in buckets:
+            buckets[key] = ver
+        else:
+            cur = buckets[key]
+            buckets[key] = ver if ver > cur else cur
+    return list(reversed(sorted(buckets.values())))
+
+
+def _find_ver_idx(vers, target):
+    for idx, ver in enumerate(vers):
+        if ver == target:
+            return idx
+    return None
+
+
+def _main():
+    builtin = Version.from_string(sys.argv[1])
+    vers = versions(json.load(sys.stdin))
+    secondlast = vers[1] if len(vers) >= 1 else vers[0]
+    out = {
+            'last': vers[0],
+            'secondlast': secondlast,
+            'builtin': builtin,
+            'previous': builtin,
+            'following': builtin,
+    }
+
+    idx = _find_ver_idx(vers, builtin)
+    if idx is not None or idx < len(builtin):
+        out['previous'] = vers[idx+1]
+    if idx is not None or idx > 1:
+        out['following'] = vers[idx-1]
+
+    print('last=%s\nsecondlast=%s\nfollowing=%s\nbuiltin=%s\nprevious=%s' % (
+          out['last'], out['secondlast'], out['following'], out['builtin'], out['previous']))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(2)
+    _main()
